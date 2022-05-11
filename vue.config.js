92f23448b865a0e251294d1cd7716a82c2ca6a7e
Bohdan Zhumabekov <ukrainskyi@gmail.com>
2022-05-11 16:15:25 +0300

    change vue.config.js && .gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/notes'
+  publicPath: process.env.NODE_ENV === "production" ? "/notes/" : "/",
 })
