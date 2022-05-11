fa8ea1946b7187f2481c8debcd7e669798cabba7
Bohdan Zhumabekov <ukrainskyi@gmail.com>
2022-05-11 15:59:08 +0300

    change vue.config.js

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:rayofgoodness/notes.git main:gh-pages
+git push -f git@github.com:rayofgoodness/notes.git master:gh-pages
 
 cd -
