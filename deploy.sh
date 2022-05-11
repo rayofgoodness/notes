7a57187b3a0b6cc8e6d55e9d0d2bc038287d27c9
Bohdan Zhumabekov <ukrainskyi@gmail.com>
2022-05-11 13:52:40 +0300

    added changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:rayofgoodness/test_project_notes.github.io.git master
+git push -f git@github.com:rayofgoodness/test_project_notes.github.io.git master:gh-pages
 
 cd -
