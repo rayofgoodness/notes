83bcf94d194550b6f86e4c7e201c9331a33cd81a
Bohdan Zhumabekov <ukrainskyi@gmail.com>
2022-05-11 13:32:22 +0300

    changes vue.config.js && build new dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'test_project_notes'
+  publicPath: '/test_project_notes/'
 })
